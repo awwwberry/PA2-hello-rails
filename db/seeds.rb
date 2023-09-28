50315ba48c5e8eea233d9f54aaa865ece3203915
Aubrey <amoulton@tamu.edu>
2023-09-28 00:09:41 +0000

    Fix syntax error

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
   {:title => 'The Matrix', :rating => 'R',
-    :release_date => '31-Mar-1999'}
+    :release_date => '31-Mar-1999'},
   {:title => 'The Sound of Music', :rating => 'G',
     :release_date => '2-Mar-1965'}
 ]
